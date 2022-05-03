3d952ed0ddf5aba66016d2c0956c6d9ee4c59cad
JenesisFWork <jenesisfablework@gmail.com>
2022-05-03 13:34:43 -0700

    Solution
    Professor's solution to the lab.

--- demo/src/main/java/com/binarysearch/MainProgram.java
+++ demo/src/main/java/com/binarysearch/MainProgram.java
@@ -3,11 +3,9 @@ package com.binarysearch;
 import java.util.*;
 
 public class MainProgram {
-
   public static void main(String[] args) {
     // create tree
     Node bst = new Node(30);
-
     // add 10 random value nodes
     final Integer max = 100;
     final Integer n = 10;
@@ -15,15 +13,12 @@ public class MainProgram {
       Integer val = (int) Math.floor(Math.random() * max);
       bst.add(val);
     }
-
     // print tree
     System.out.println("Printing the tree using in-order");
     bst.printInOrder();
-
     // main loop for app
     Scanner scan = new Scanner(System.in);
     String command = "_";
-
     // loop until user quits
     while (!command.equals("q")) {
       System.out.println();
@@ -31,33 +26,27 @@ public class MainProgram {
       command = menuGetCommand(scan);
       if (!command.equals("q")) {
         Integer val = Integer.parseInt(command);
-
         // do binary search in the bst
         System.out.printf("Searching for the value %d\n", val);
         // student needs to write code to
         // search tbe binary tree by calling Node.java's "search" method.
         // the variable "val" has the integer value to be searched for.
-
+        Node search = bst.search(val);
         // student needs to write code to
         // then print results of the search
+        if (search == null) {
+          System.out.printf("The value %d is not in the tree\n", val);
+        } else {
+          System.out.printf("The value %d is in the tree\n", val);
+        }
       }
-        if (!command.equals(max)) {
-        Integer val = Integer.parseInt(command);
-        System.out.printf("The value %d\n is in the tree", val);
-        }
-        else {
-        System.out.printf("The value %d\n is not in the tree");
-        }
     }
   }
-
   // get first character from input
   private static String menuGetCommand(Scanner scan) {
     String command = "_";
-
     String rawInput = scan.nextLine();
     command = rawInput.toLowerCase();
-
     return command;
   }
 }
--- demo/src/main/java/com/binarysearch/Node.java
+++ demo/src/main/java/com/binarysearch/Node.java
@@ -1,24 +1,19 @@
 package com.binarysearch;
-
 // node for a binary search tree
 class Node {
-
   int value;
   Node left;
   Node right;
-
   // constructor
   Node(int value) {
     this.value = value;
     right = null;
     left = null;
   }
-
   // getter for the value of the node
   public int value() {
     return value;
   }
-
   // add a node
   public void add(int newValue) {
     // add to left
@@ -29,7 +24,6 @@ class Node {
         left.add(newValue);
       }
     }
-
     // add to right
     if (newValue > value) {
       if (right == null) {
@@ -39,9 +33,7 @@ class Node {
       }
     }
     // if curr.value == newValue, we have already added it, do nothing
-
   }
-
   // print, in-order left-node-right (sorted) tree traversal
   private void printInOrder(Node curr) {
     if (curr != null) {
@@ -50,17 +42,39 @@ class Node {
       printInOrder(curr.right);
     }
   }
-
   // print
   public void printInOrder() {
     printInOrder(this);
   }
-
   // find a value in the tree
   // student needs to write code to
   // do a binary search of the BST tree
   public Node search(int x) {
-    System.out.printf("does nothing ...\n");
+    if (value == x) {
+      return this;
+    }
+    // search left
+    if ((x < value) && (left != null)) {
+      return left.search(x);
+    }
+    // search right
+    if ((x > value) && (right != null)) {
+      return right.search(x);
+    }
     return null;
+  }
+  // print, revesre in-order
+  // student needs to write code to
+  // do a revesre in-order traversal of the tree
+  public void printReverseInOrder(Node curr) {
+    if (curr != null) {
+      printReverseInOrder(curr.right);
+      System.out.printf("node value = %d\n", curr.value);
+      printReverseInOrder(curr.left);
+    }
+  }
+  // print, revesre in-order
+  public void printReverseInOrder() {
+    printReverseInOrder(this);
   }
 }
